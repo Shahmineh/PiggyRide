ccbbcff2becc8dd8349a5489d2fce15a6073c82e
Tin Kotvas <tin.kotvas@gmail.com>
2018-03-07 14:29:26 +0100

    added waypoints and popstatehandler classes

---
+++ www/src/js/popstatehandler.class.js
@@ -0,0 +1,82 @@
+class PopStateHandler {
+
+    // Note: Only instantiate PopStateHandler once!
+  
+    constructor(){
+      // Add event handlers for a.pop-links once
+      this.addEventHandler();
+      // Call changePage on initial page load
+      this.changePage();
+      // Call changePage on pop events
+      // (the user clicks the forward or backward button)
+      // from an arrow function to keep "this"
+      // inside changePage pointing to the PopStateHandler object
+      window.addEventListener('popstate', () => this.changePage());
+  
+    }
+  
+    addEventHandler(){
+  
+      // make "that" the PopStateHandler object
+      // (since this will be the a tag inside the click function)
+      let that = this;
+  
+      $(document).on('click','a.pop',function(e){
+  
+        // Create a push state event
+        let href = $(this).attr('href');
+        history.pushState(null, null, href);
+  
+        // Call the changePage function
+        that.changePage();
+  
+        // Stop the browser from starting a page reload
+        e.preventDefault();
+  
+      });
+    }
+  
+    changePage(){
+      // React on page changed
+      // (replace part of the DOM etc.)
+  
+      // Get the current url
+      let url = location.pathname;
+  
+      // Change which menu link that is active
+      $('header a').removeClass('active');
+      $(`header a[href="${url}"]`).addClass('active');
+  
+      // A small "dictionary" of what method to call
+      // on which url
+      let urls = {
+        '/': 'start',
+        '/spel': 'spel',
+        '/kontakt': 'contact'
+      };
+  
+      // Call the right method
+      let methodName = urls[url] || 'start';
+      this[methodName]();
+  
+    }
+  
+    start(){
+      $('main').html(`
+        <h1>Välkommen</h1>
+        <p>Du är på min fina startsida</p>
+      `);
+    }
+  
+    spel(){
+      $('main').html('Du vill spela ett spel!');
+    }
+  
+    contact(){
+      $('main').html('Här finns kontaktuppgifter!');
+    }
+  
+  }
+  
+  // Create an instance of the class
+  new PopStateHandler();
---
+++ www/src/js/waypoints.class.js
@@ -0,0 +1,82 @@
+const request = require('request-promise-native');
+
+module.exports = class Waypoints {
+
+  constructor(obj) {
+    Object.assign(this, obj);
+    let leg = this.gway.routes[0].legs[0];
+    this.startAddress = leg.start_address;
+    this.endAdress = leg.end_address;
+    this.duration = leg.duration.value;
+    this.durationMinutes = Math.round(this.duration / 60);
+    this.endTime = new Date(this.startTime.getTime() + this.duration * 1000);
+    this.distance = leg.distance.value;
+    this.speed = Math.round((this.distance / 1000) / (this.durationMinutes / 60));
+    this.positions = [
+      Object.assign({
+          time: this.startTime
+        },
+        leg.start_location, {
+          text: 'Du är på ' + this.startAddress
+        }
+      )
+    ];
+    let time = this.startTime;
+    for (let step of leg.steps) {
+      time = new Date(time.getTime() + step.duration.value * 1000);
+      this.positions.push({
+        time: time,
+        lat: step.end_location.lat,
+        lng: step.end_location.lng,
+        text: step.html_instructions
+      });
+    }
+    // this.steps = leg.steps;
+    delete this.gway;
+  }
+
+  get hqPos() {
+    // Nordenskiöldsgatan 13, Malmö
+    return {
+      lat: 55.6108096,
+      lng: 12.9946562
+    }
+  }
+
+  async position(timeString) {
+    let time = new Date(timeString),
+        timeDiff = Math.abs(time - this.positions[0].time),
+        currPos = {};
+
+    for (let position of this.positions) {
+      let currDiff = Math.abs(time - position.time);
+      if (currDiff <= timeDiff) {
+        timeDiff = currDiff;
+        currPos = position;
+      }
+    }
+    return currPos
+  }
+
+  static async create(obj) {
+
+    let url = 'https://maps.googleapis.com/maps/api/directions/json';
+
+    let params = {
+      origin: obj.from + (obj.from.includes('Malmö') ? '' : ' Malmö'),
+      destination: obj.to + (obj.to.includes('Malmö') ? '' : ' Malmö'),
+      mode: obj.how || 'walking',
+      key: 'AIzaSyBrE71qz1iEQgNnM3fbzIXB5pmRoB9rWiQ',
+      language: 'sv'
+    };
+
+    for (let param in params) {
+      url += url.includes('?') ? '&' : '?';
+      url += param + '=' + encodeURIComponent(params[param]);
+    }
+
+    obj.gway = JSON.parse(await request(url));
+    return new this(obj);
+  }
+
+}
