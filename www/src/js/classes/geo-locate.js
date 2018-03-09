6c34df625d6b93f6e5b3fc14b792b1e6d7cf0baf
Ahmet Topic <ahmettopic@hotmail.com>
2018-03-09 14:49:26 +0100

    final för geoloc.
    getLocation ger endast long/lat

--- www/src/js/classes/geo-locate.js
+++ www/src/js/classes/geo-locate.js
@@ -1,12 +1,12 @@
-export default function geoEvent() {
-  $(document).on("click", "#getLocation", function () {
-    console.log("boom")
+export default function geoEvent () {
+  $(document).on('click', '#getLocation', function () {
+    console.log('boom');
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
       });
     } else {
-      this.innerHTML = "Geolocation is not supported by this browser.";
+      this.innerHTML = 'Geolocation is not supported by this browser.';
     }
   });
-};
+}
