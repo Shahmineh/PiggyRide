a24864583d8c39a05095e8e79291f6441fb8915b
Ahmet Topic <ahmettopic@hotmail.com>
2018-03-09 10:42:08 +0100

    add

---
+++ www/src/js/classes/google-auto-complete.js
@@ -0,0 +1,6 @@
+export default function initAutocomplete() {
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: 'roadmap'
+  });
--- www/src/js/main.js
+++ www/src/js/main.js
@@ -3,6 +3,9 @@ import jQuery from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import App from './classes/app.class.js';
+import initAutocomplete from './classes/google-auto-complete.js';
+
+
 
 // @ts-ignore
 window.$ = window.jQuery = jQuery; // Sets $ to global
