4c82bafeb2ddd882d5cc92aaa58fd74efdfe6f96
Dacrol <jonas.ahrens@gmail.com>
2018-03-11 02:54:52 +0100

    Add easings

--- www/src/js/ui/scroll.js
+++ www/src/js/ui/scroll.js
@@ -1,3 +1,6 @@
+// @ts-ignore
+require('jquery-ui/ui/effect');
+
 /**
  * Assigns the scrollTo method to jQuery.
  *
@@ -8,12 +11,13 @@ function assignScrollTo ($) {
     throw new Error('$ not found.');
   }
   Object.assign($, {
-    scrollTo: function (target, time = 2000) {
+    scrollTo: function (target, time = 1500, easing = 'swing') {
       $('html, body').animate(
         {
           scrollTop: $(target).offset().top
         },
-        time
+        time,
+        easing
       );
       // console.log('Scrolled');
     }
