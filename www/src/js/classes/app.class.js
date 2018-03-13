1226b96c6ae3353833f0393c1dfe7e91e295b323
Tin <tin.kotvas@gmail.com>
2018-03-14 00:42:47 +0100

    adjusted table added bootstrap-sortable

--- www/src/js/classes/app.class.js
+++ www/src/js/classes/app.class.js
@@ -1,11 +1,13 @@
 import PopStateHandler from './pop-state-handler.class';
-import Order from './order.class';
+
+// classes
+import REST from './REST.class';
 
 export default class App extends PopStateHandler {
   initialize () {
     const app = this;
 
-    app.bindView('home.html', '/', null, () => {
+    app.bindView('home.html', '/home', null, () => {
       $('#departure-time').datetimepicker({
         locale: 'sv'
       });
@@ -21,8 +23,35 @@ export default class App extends PopStateHandler {
 }
 
 async function getOrders () {
-  let orders = await Order.find({});
+  let orders = await REST.request('orders', 'GET', {});
+  orders = orders.result || [orders];
+
   console.log(orders);
 
-  // let ordersHead = $('#orders thead')[0];
+  let html = $('<div class="col-10 mx-auto"></div>');
+  let table = $('<table id="orders" class="table"></table>');
+  let tbody = $('<tbody></tbody>');
+  let thead = $(`<thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">ID</th>
+                      <th scope="col">Total Price</th>
+                      <th scope="col">Order time</th>
+                    </tr>
+                  </thead>`);
+
+  for (let order of orders) {
+    let tr = $(`<tr> 
+                <th>${order._id}</th>
+                <th>${order.totalPrice}</th>
+                <th>${order.orderTime}</th>
+              </tr>`);
+
+    tr.appendTo(tbody);
+  }
+  thead.appendTo(table);
+  tbody.appendTo(table);
+  table.appendTo(html);
+
+  $('body').append(html);
 }
