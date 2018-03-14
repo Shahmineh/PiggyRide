747765bedbab88149426115203226ceef1c35256
Johan <jmiklo@outlook.com>
2018-03-14 15:20:56 +0100

    updates for login/user reg

--- www/src/js/classes/user.class.js
+++ www/src/js/classes/user.class.js
@@ -14,8 +14,11 @@ export default class User extends REST {
       email = $('#loginUsername').val();
       password = $('#loginPassword').val();
       let nameResult = await User.create({ email: email, passwordHash: password});
-      console.log('nameResult', nameResult);
-
+      // console.log('nameResult.error', nameResult.error)
+      if (nameResult.error) {
+        // console.log('Denna användare existerar redan!');
+        $(".errormsg").append("<p class='warning'> Username allready exists! </p>");
+      }
     });
   }// end eventhandler
 
