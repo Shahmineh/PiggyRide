86f726fb72daf5da27374a962372897daceaf91c
Dacrol <jonas.ahrens@gmail.com>
2018-03-14 23:28:17 +0100

    Format

--- www/src/js/classes/user.class.js
+++ www/src/js/classes/user.class.js
@@ -1,25 +1,28 @@
 import REST from './REST.class.js';
 
 export default class User extends REST {
-
-  constructor(user){
+  constructor (user) {
     super(user);
     this.eventHandlers();
   }
 
-  eventHandlers(){
+  eventHandlers () {
     let email, password;
-    
-    $(document).on('click', '#signupbtn', async ()=> {
+
+    $(document).on('click', '#signupbtn', async () => {
       email = $('#loginUsername').val();
       password = $('#loginPassword').val();
-      let nameResult = await User.create({ email: email, passwordHash: password});
+      let nameResult = await User.create({
+        email: email,
+        passwordHash: password
+      });
       // console.log('nameResult.error', nameResult.error)
       if (nameResult.error) {
         // console.log('Denna användare existerar redan!');
-        $(".errormsg").append("<p class='warning'> Username allready exists! </p>");
+        $('.errormsg').append(
+          "<p class='warning'> Username allready exists! </p>"
+        );
       }
     });
-  }// end eventhandler
-
-}// end class
+  } // end eventhandler
+} // end class
