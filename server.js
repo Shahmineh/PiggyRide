e9782300c1d1f52d96af006fa45d6e35fe564422
Rickard <rickard_thomsen@hotmail.com>
2018-03-18 15:03:41 +0100

    testing out sessions

--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const path = require('path');
 const validRoutes = [];
 Object.assign(app, { validRoutes: validRoutes });
 
+const Session = require('./backend/session.class');
+function session (req, res, next) {
+  if (!req.session) {
+    // set a cookie for a session if it doesn't exist
+    let MySessionModel = new Session(app).myModel;
+    // see https://expressjs.com/en/4x/api.html#res.cookie
+    let mySession = new MySessionModel({
+      loggedIn: true
+    });
+    res.cookie('session', mySession._id, {
+      path: '/',
+      httpOnly: true
+    });
+    // save our new cookie to our new session
+    mySession.save(e => {});
+    req.session = mySession;
+    console.log(req.session);
+  } else {
+
+
+
+
+  }
+  next();
+}
+
 app.use(bodyParser.json());
 app.use(express.static('www'));
+app.use(session);
 
 const Order = require('./backend/order.class');
 let order = new Order(app);
@@ -15,8 +42,7 @@ let order = new Order(app);
 const Piggy = require('./backend/piggy.class');
 let piggy = new Piggy(app);
 
-const Session = require('./backend/session.class');
-let session = new Session(app);
+// let session = new Session(app);
 
 const User = require('./backend/user.class');
 let user = new User(app);
@@ -35,7 +61,7 @@ app.get(/^[^.]*$/, (req, res, next) => {
   let reqPath = req.path.split('/').slice(1);
   if (
     reqPath[0] &&
-    !validRoutes.some((route) => {
+    !validRoutes.some(route => {
       return reqPath[0] === route || reqPath[0].startsWith(route + '/'); // route.slice(0, -1) to match singulars
     })
   ) {
