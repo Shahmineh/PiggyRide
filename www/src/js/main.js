309a9d362b350a3ec5a62bf952769d912808dc8e
Dacrol <jonas.ahrens@gmail.com>
2018-03-19 02:27:02 +0100

    Combine orders and map on admin page

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,7 +1,5 @@
 import 'babel-polyfill';
 import jQuery from 'jquery';
-import 'popper.js';
-import 'bootstrap';
 import App from './classes/app.class.js';
 import moment from 'moment';
 import 'moment/locale/sv';
@@ -12,6 +10,13 @@ import REST from './classes/REST.class.js';
 window.$ = window.jQuery = jQuery; // Sets $ to global
 // @ts-ignore
 window.moment = moment;
+require('bootstrap');
+require('popper.js');
+// @ts-ignore
+// require('bootstrap-table');
+// @ts-ignore
+// require('bootstrap-table/dist/locale/bootstrap-table-sv-SE')
+
 require('./ui/scroll.js'); // Assigns the scrollTo method to jQuery
 // @ts-ignore
 require('tempusdominus-bootstrap-4');
