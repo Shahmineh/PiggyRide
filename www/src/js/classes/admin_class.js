7db0f9156ee4d33e68cac77556939cbcee8142e1
Johan <jmiklo@outlook.com>
2018-03-19 13:53:35 +0100

    started with admin functions

---
+++ www/src/js/classes/admin_class.js
@@ -0,0 +1,23 @@
+import REST from './REST.class.js';
+
+export default class Admin extends REST {
+  constructor (user) {
+    super(user);
+    User.eventsInitialized = User.eventsInitialized || this.setupEventHandlers();
+  }
+
+  /* setupEventHandlers () {
+    $(document).on('click', '#listallusers', async () => {
+      console.log('CLICK');
+      let email;
+
+      let allUsers = await Admin.find(email: email);
+      console.log('allUsers', allUsers);
+     
+     
+     
+    });
+    return true;
+  } // end eventhandler
+
+} // end class */
