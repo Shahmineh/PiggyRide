dc2b8800754b22e4edb76f3452dbdcd05141b3c1
Rickard <rickard_thomsen@hotmail.com>
2018-03-20 19:48:51 +0100

    Added User Model from server.js and  and fixed else state

--- backend/helper.class.js
+++ backend/helper.class.js
@@ -1,10 +1,11 @@
 const Session = require('./session.class');
+const User = require('./user.class');
 
 module.exports = class myHandler {
-  constructor (app) {
+  constructor (app, userModel) {
     this.expressApp = app;
+    this.UserModel = userModel;
     this.MySessionModel = new Session(this.expressApp).myModel;
-    // console.log(this.MySessionModel)
     return this.session.bind(this);
   }
 
@@ -12,7 +13,7 @@ module.exports = class myHandler {
     // console.log(res);
     if (!req.cookies.session) {
       // set a cookie for a session if it doesn't exist
-      //här vill jag skapa en användare
+      // här vill jag skapa en användare
       // see https://expressjs.com/en/4x/api.html#res.cookie
       // console.log(this)
       let mySession = new this.MySessionModel();
@@ -20,35 +21,36 @@ module.exports = class myHandler {
         path: '/',
         httpOnly: true
       });
-      mySession.set({
-        loggedIn: true,
-        data: {
-          user: 'test'
-        }
-      });
+      // mySession.set({
+      //   loggedIn: true,
+      //   data: {
+      //     user: 'test'
+      //   }
+      // });
 
       // save our new cookie to our new session
       mySession.save();
       req.session = mySession;
+
     } else {
-      //här hämtar vi en användare
-      let sessions = await this.MySessionModel.find({ _id: req.cookies.session });
-      if (sessions[0]) {
-        req.session = sessions[0];
-        // console.log(req.session);
-        // req.session.data = req.session.data || {};
-        //  // is there a userId saved on the session and are they logged in?
-        // if(req.session.data.userId && req.session.loggedIn){
-        //   let users = await User.find({_id: req.session.data.userId});
-        //   if(users[0]){
-        //     req.user = users[0]; // apply the user object
-        //   }
-        // }
+
+      let session = await this.MySessionModel.findOne({ _id: req.cookies.session });
+
+      if (session) {
+        req.session = session;
+        req.session.data = req.session.data || {};
+        // console.log(req.session.data)
+        // is there a userId saved on the session and are they logged in?
+        if (req.session.data.userId && req.session.loggedIn) {
+          let user = await this.UserModel.findOne({ _id: req.session.data.userId });
+          if (user) {
+            req.user = user; // apply the user object
+          }
+        }
+      } else {
+        delete req.cookies.session;
+        // return session(req, res, next);
       }
-      // else{
-      //   delete(req.cookies.session);
-      //   return session(req, res, next);
-      // }
     }
     next();
   }
