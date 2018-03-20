539bca348ceb202bc40e6d0d886df5c2282d91d9
Rickard <rickard_thomsen@hotmail.com>
2018-03-20 21:32:54 +0100

    Added so u can sign in

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const qs = require('qs');
 const cookieParser = require('cookie-parser');
 const MyHandler = require('./backend/helper.class');
 const validRoutes = [];
 Object.assign(app, { validRoutes: validRoutes });
 
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 app.use(express.static('www'));
 app.use(cookieParser());
@@ -26,8 +32,6 @@ const Piggy = require('./backend/piggy.class');
 let piggy = new Piggy(app);
 
 // let session = new Session(app);
-
-
 
 const Extra = require('./backend/extra.class');
 let extra = new Extra(app);
@@ -54,21 +58,28 @@ app.get(/^[^.]*$/, (req, res, next) => {
 });
 
 app.post('/login', async (req, res) => {
-  let currentUser = await UserModel.findOne({
-    email: req.body.email,
-    passwordHash: req.body.passwordHash
-  });
 
-  if (currentUser) {
-    currentUser.sessionID = req.cookies.session;
-    req.session.data.userId = currentUser._id;
-    req.session.loggedIn = true;
-    currentUser.save();
-    console.log(req.session);
-    res.json(currentUser);
-  } else {
-    res.json('You need to create an account');
+  let obj = req.body;
+  for (let key in obj) {
+    let parsedData = JSON.parse(key);
+    // console.log(parsedData.email)
+    let currentUser = await UserModel.findOne({
+      email: parsedData.email,
+      passwordHash: parsedData.passwordHash
+    });
+    if (currentUser) {
+      currentUser.sessionID = req.cookies.session;
+      req.session.data = { userId: currentUser._id };
+      req.session.loggedIn = true;
+      currentUser.save();
+
+      res.json(currentUser);
+    } else {
+      res.json('You need to create an account');
+    }
   }
+
+
   // let User = await UserModel.find({_id : req.cookies.session});
 });
 
@@ -108,7 +119,7 @@ app.all('/sign-out', async (req, res) => {
   req.user = {};
   req.session.loggedIn = false;
   let result = await req.session.save();
-  res.json({message: 'Logged out', session: req.session, user: req.user});
+  res.json({ message: 'Logged out', session: req.session, user: req.user });
 });
 
 app.listen(3000, () => {
--- www/src/js/classes/user.class.js
+++ www/src/js/classes/user.class.js
@@ -3,7 +3,8 @@ import REST from './REST.class.js';
 export default class User extends REST {
   constructor (user) {
     super(user);
-    User.eventsInitialized = User.eventsInitialized || this.setupEventHandlers();
+    User.eventsInitialized =
+      User.eventsInitialized || this.setupEventHandlers();
   }
 
   setupEventHandlers () {
@@ -12,31 +13,65 @@ export default class User extends REST {
     $(document).on('click', '#signupbtn', async () => {
       email = $('#loginUsername').val();
       password = $('#loginPassword').val();
-      
-      let nameResult = await User.create({ email: email, passwordHash: password});
-       console.log('nameResult.error', nameResult.error)
-      if (nameResult.error) {
-        // console.log('Denna användare existerar redan!');
-        $('err')
-        $(".errormsg").html("<p class='danger'> Denna emailadress är redan registrerad! </p>");
-        }
-        else {
-          $(".errormsg").html("<p class='success'> Tack för att du registrerat dig hos oss! </p>");
-        }
+
+      // fetch('http://localhost:3000/login', {
+      //   method: 'POST', // or 'PUT'
+      //   body: JSON.stringify({
+      //     email: email,
+      //     passwordHash: password
+      //   }),
+      //   headers: new Headers({
+      //     'Content-Type': 'application/json'
+      //   })
+      // })
+      //   .then(res => {
+      //     console.log(res);
+      //   })
+      //   .catch(error => console.error('Error:', error))
+      //   .then(response => console.log('Success:', response));
+
+      // let nameResult = await User.create({ email: email, passwordHash: password});
+      //  console.log('nameResult.error', nameResult.error)
+      // if (nameResult.error) {
+      //   // console.log('Denna användare existerar redan!');
+      //   $('err')
+      //   $(".errormsg").html("<p class='danger'> Denna emailadress är redan registrerad! </p>");
+      //   }
+      //   else {
+      //     $(".errormsg").html("<p class='success'> Tack för att du registrerat dig hos oss! </p>");
+      //   }
     });
 
-    $(document).on('click', '#loginbtn', async ()=> {
+    $(document).on('click', '#loginbtn', async () => {
       email = $('#loginUsername').val();
       password = $('#loginPassword').val();
-      let nameResult = await User.find({ email: email, passwordHash: password});
-      if (nameResult === undefined || nameResult.length == 0) {
-        $(".errormsg").html("<p class='danger'> Vänligen kontrollera att du skrivit rätt emailadress och lösenord! </p>");
-      } 
+      // let nameResult = await User.find({
+      //   email: email,
+      //   passwordHash: password
+      // });
+      // if (nameResult === undefined || nameResult.length == 0) {
+      //   $('.errormsg').html(
+      //     "<p class='danger'> Vänligen kontrollera att du skrivit rätt emailadress och lösenord! </p>"
+      //   );
+      // }
+      let data = {
+        email: email,
+        passwordHash: password
+      };
+      let newData = JSON.stringify(data);
+
+
+      $.ajax({
+        url: '/login',
+        type: 'POST',
+        data: newData
+      })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log('fewfewfw', err);
+        });
     });
-
-
-  }// end eventhandler
-
-
-
-}// end class
+  } // end eventhandler
+} // end class
