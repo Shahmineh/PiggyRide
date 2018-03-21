beda125358409933328ddd1a5d7aa7845469d3c6
Dacrol <jonas.ahrens@gmail.com>
2018-03-21 23:20:15 +0100

    Finalize best piggy UI

--- www/src/js/ui/find-piggy.js
+++ www/src/js/ui/find-piggy.js
@@ -15,14 +15,38 @@ async function findPiggy (type = piggyType) {
     .children()
     .first()
     .val();
-  let from = $('#autocompleteTo').val()
+  let from = $('#autocompleteTo').val();
   if (type && time && from) {
-    let req = {from: from, time: time}
-    let response = await $.ajax({url: '/bestpiggy', type: 'GET', data: req})
+    let req = { from: from, time: time };
+    let response = await $.ajax({ url: '/bestpiggy', type: 'GET', data: req });
     console.log(response);
-    let bestPiggy = response.filter((wps) => wps.piggy.type === type).map(wps => Object.assign(wps, {startTime: new Date(wps.startTime), endTime: new Date(wps.endTime)})).sort((a, b) => a.endTime - b.endTime);
+    let bestPiggy = response
+      .filter(wps => wps.piggy.type === type)
+      .map(wps =>
+        Object.assign(wps, {
+          startTime: new Date(wps.startTime),
+          endTime: new Date(wps.endTime)
+        })
+      )
+      .sort((a, b) => a.endTime - b.endTime)[0];
     // console.log(bestPiggy)
-    return bestPiggy[0].endTime;
+    const dateOptions = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZone: 'Europe/Stockholm'
+    };
+    let bestTime =
+      bestPiggy.startTime < new Date().getTime() + bestPiggy.duration * 1000
+        ? bestPiggy.endTime
+        : bestPiggy.startTime;
+    $('#next-time')
+      .text(bestTime.toLocaleDateString('sv-SE', dateOptions))
+      .parent()
+      .show();
+    return bestPiggy.startTime;
   }
 }
 
@@ -32,6 +56,6 @@ async function findPiggy (type = piggyType) {
   day: 'numeric',
   hour: 'numeric',
   minute: 'numeric',
-  timeZone: 'Sweden/Stockholm'
+  timeZone: 'Europe/Stockholm'
 };
 .toLocaleDateString('sv-SE', dateOptions) */
