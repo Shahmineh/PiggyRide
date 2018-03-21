d52e4fe8324458ecc5a54ed2cc4bf45c54c2a232
Dacrol <jonas.ahrens@gmail.com>
2018-03-21 20:42:35 +0100

    Add logout button and functionality

--- www/src/js/ui/check-login.js
+++ www/src/js/ui/check-login.js
@@ -1,7 +1,32 @@
-import User from '../classes/user.class'
+import User from '../classes/user.class';
 
-export default async function checkLogin () {
+export default async function checkLogin (res) {
   let user = await User.findOne('');
   console.log(user);
+  if (user) {
+    $('#login-form').hide();
+    $('#logout-form').show();
+  } else {
+    $('#login-form').show();
+    $('#logout-form').hide();
+  }
+  $('#logout-form').off('click')
+  $('#logout-form').on('click', function () {
+    $.ajax({
+      url: '/sign-out',
+      type: 'POST'
+    })
+      .then(res => {
+        checkLogin();
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  });
+  if (res && res.loggedIn && !user) {
+    // MongoDB did not have time to finish saving the last request so try once more
+    setTimeout(() => {
+      checkLogin();
+    }, 2000);
+  }
 }
-
